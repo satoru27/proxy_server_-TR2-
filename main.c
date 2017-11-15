a5ded3cc9ab7383668d3932c9cd026ea9899327a
Vitor Satoru <vitorsatoru@hotmail.com>
2017-11-15 21:53:23 -0200

    main adequado para rodar apenas como servidor tcp

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 * Disciplina: Teleinformática e Redes 2
 * Turma: A
 * Professor: Joao Jose Costa Gondim
-* Alunos: Vitor Satoru Machi Matsumine, Pedro Aurélio Coelho de Almeia
-* Matriculas: 14/0033262, 14/0158103
+* Alunos: Vitor Satoru Machi Matsumine, Pedro Aurélio Coelho de Almeia, Jonas Prado Soares
+* Matriculas: 14/0033262, 14/0158103, 13/0117277
 *
  */
 
@@ -18,116 +18,35 @@
 
 int main(int argc, char *argv[]){
 
-  char *mode,*protocol = "-t";
   long int temp,port = 33333;
   int return_flag;
 
-  switch(argc){
+  if(argc < 2){
+    printf("[!] More arguments are needed, exiting...\n");
+    return -1;
+  }
+  
+  if(argc > 2){
+    printf("[!] Too many arguments (max = 1), exceeding arguments were ignored\n");
+  }
 
-    case 1:
-      printf("[!] More arguments are needed, exiting...\n");
-      return -1;
+  temp = strtol(argv[1],NULL,0);
 
-    case 2:
-      /*
-      if(strcmp(argv[1],"-S")!=0 && strcmp(argv[1],"host")!=0){
-
-
-        printf("[!] Invalid mode, exiting...\n");
-        return -1;
-      }
-      */
-        mode=argv[1];
-        break;
-
-    case 3:
-/*
-      if(strcmp(argv[1],"-S")!=0 && strcmp(argv[1],"host")!=0 ){
-        printf("[!] Invalid mode, exiting...\n");
-        return -1;
-      }
-*/
-      mode=argv[1];
-      temp = strtol(argv[2],NULL,0);
-      if(temp < 1025){
-        printf("[!] Invalid port, default port (33333) was selected\n");
-      }
-      else{
-      port=temp;
-      }
-      break;
-
-    default:
-      if(argc > 4){
-        printf("[!] Too many arguments (max = 3), exceeding arguments were ignored\n");
-      }
-      /*
-      if(strcmp(argv[1],"-S")!=0 && strcmp(argv[1],"host")!=0 ){
-        printf("[!] Invalid mode, exiting...\n");
-        return -1;
-      }
-      */
-      mode=argv[1];
-
-      temp = strtol(argv[2],NULL,0);
-
-      if(temp < 1025){
-        printf("[!] Invalid port, default port (33333) was selected\n");
-      }
-      else{
-      port=temp;
-      }
-
-      if(strcmp(argv[3],"-t")!=0 && strcmp(argv[3],"-u")!=0){
-        printf("[!] Invalid selected protocol, using default protocol (UDP)\n");
-      }
-      else{
-        protocol = argv[3];
-      }
-      break;
-
-    }
-
-  if(strcmp(mode,"-S")==0){
-    if(strcmp(protocol,"-t")==0){
-      printf("[.] Operating mode: server\n");
-      printf("[.] Port: %li\n",port);
-      printf("[.] Protocol: TCP\n");
-      printf("------------------------------------\n");
-      return_flag = run_tcp_server(port);
-    }
-    else{
-      // printf("[.] Operating mode: server\n");
-      // printf("[.] Port: %li\n",port);
-      // printf("[.] Protocol: UDP\n");
-      // printf("------------------------------------\n");
-      // return_flag = run_udp_server(port);
-    	printf("[!] NO UDP MODE\n");
-    }
+  if(temp < 1025){
+    printf("[!] Invalid port, default port (33333) was selected\n");
   }
   else{
-    if(strcmp(protocol,"-t")==0){
-      printf("[.] Operating mode: client\n");
-      printf("[.] Port: %li\n",port);
-      printf("[.] Protocol: TCP\n");
-      printf("------------------------------------\n");
-      return_flag = run_tcp_client(mode,port);
-    }
-    else{
-      // printf("[.] Operating mode: client\n");
-      // printf("[.] Port: %li\n",port);
-      // printf("[.] Protocol: UDP\n");
-      // printf("------------------------------------\n");
-      // return_flag = run_udp_client(mode,port);
-         printf("[!] NO UDP MODE\n");
-
-    }
+    port=temp;
   }
+
+  printf("[.] Operating mode: server\n");
+  printf("[.] Port: %li\n",port);
+  printf("[.] Protocol: TCP\n");
+  printf("------------------------------------\n");
+  return_flag = run_tcp_server(port);
 
   printf("[*] Program exited with flag: %d\n",return_flag);
   return return_flag;
 
-
 //spinning_thing(1);
-
 }
